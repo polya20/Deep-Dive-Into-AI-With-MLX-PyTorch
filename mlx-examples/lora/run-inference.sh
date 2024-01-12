2dc2b18455080fe8c4ec7a9960ab0a58cee5dbab
Wankyu Choi <wankyuchoi@gmail.com>
2024-01-13 03:02:12 +0900

    updated

--- mlx-examples/lora/run-inference.sh
+++ mlx-examples/lora/run-inference.sh
@@ -1,4 +1,4 @@
-python lora.py --model /Users/wankyuchoi/cwk-llm-models/mistral-7B-v0.1-mlx-loar\
+python lora.py --model /Users/wankyuchoi/cwk-llm-models/mistral-7B-v0.1-mlx-lora\
                --adapter-file ./adapters.npz \
                --max-tokens 50 \
                --temp 0.8 \

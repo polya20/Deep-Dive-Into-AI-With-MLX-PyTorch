1fd265485d15acd4289c146f7120390ad1d85fda
Wankyu Choi <wankyuchoi@gmail.com>
2024-01-13 02:48:26 +0900

    updated

--- mlx-examples/lora/run-inference.sh
+++ mlx-examples/lora/run-inference.sh
@@ -1,6 +1,6 @@
-python lora.py --model /Users/wankyuchoi/cwk-llm-models/mistral-7B-v0.1-mlx\
+python lora.py --model /Users/wankyuchoi/cwk-llm-models/mistral-7B-v0.1-mlx-loar\
                --adapter-file ./adapters.npz \
-               --num-tokens 50 \
+               --max-tokens 50 \
                --temp 0.8 \
                --prompt "Q: What is relu in mlx?
 A: "
